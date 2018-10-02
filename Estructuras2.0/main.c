0555de3bb57ecbff89dc8d216b62a050aa7c4951
Gonzalomatias <42710431+Gonzalomatias@users.noreply.github.com>
2018-10-01 21:16:04 -0300

    Add files via upload

--- Estructuras2.0/main.c
+++ Estructuras2.0/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define TAM 10
 
 typedef struct
@@ -7,26 +8,40 @@ typedef struct
     int legajo;
     char nombre[20];
     char sexo;
+    int idSector;
     float sueldo;
     int isEmpty;
 
 }eEmpleados;
 
+typedef struct
+{
+    int id;
+    char descripcion[20];
+}eSector;
+
 
 int menu();
 void inicializarEmpleados(eEmpleados empleados[],int tam);
-void mostrarEmpleado(eEmpleados unEmpleado);
-void mostrarEmpleados(eEmpleados lista[],int tam);
+void mostrarEmpleado(eEmpleados unEmpleado,eSector sectores,int tam);
+void mostrarEmpleados(eEmpleados lista[],int tam,eSector sectores,int tamSector);
 int buscarLibre(eEmpleados lista[],int tam);
 int buscarEmpleado(eEmpleados lista[],int tam,int legajo);
-void altaEmpleado(eEmpleados lista[],int tam);
+void altaEmpleado(eEmpleados lista[],int tam,eEmpleados sectores[],int tamSector);
 void bajaEmpleado(eEmpleados lista[],int tam);
 void modificarEmpleado(eEmpleados lista[],int tam);
+void ordenarEmpleadosXlegajo(eEmpleado lista[], int tam);
+void harcodearEmpleados(eEmpleados empleados[]);
+void harcodearSectores{eSector sectores[]);
+void obtenerSector(eSector sectores[],int tam,int idSector,char cadena[]);
 
 int main()
 {
+    eSector sectores[TAM];
     eEmpleados empleado[TAM];
     inicializarEmpleados(empleado,TAM);
+    harcodearEmpleados(empleado);
+    harcodearSectores(empleado);
 
     char seguir='s';
 
@@ -46,11 +61,10 @@ int main()
                 break;
             case 4:
                 printf("\t**Listar**\n");
-                mostrarEmpleados(empleado,TAM);
+                mostrarEmpleados(empleado,TAM,sectores,5);
                 break;
             case 5:
                 printf("\t**Ordenar**\n");
-
                 break;
             case 6:
                 seguir = 'n';
@@ -99,13 +113,16 @@ void inicializarEmpleados(eEmpleados empleados[],int tam)
 }
 
 
-void mostrarEmpleado(eEmpleados unEmpleado)
+void mostrarEmpleado(eEmpleados unEmpleado,eSector sectores,int tam)
 {
-   printf("%d\t \t%s \t%c \t%.2f",unEmpleado.legajo,unEmpleado.nombre,unEmpleado.sexo,unEmpleado.sueldo);
+    char descSector[20];
+    obtenerSector(sectores,tam,unEmpleado.idSector,descSector);
+
+   printf("%d\t \t%s \t%c \t%.2f \t%10s\n",unEmpleado.legajo,unEmpleado.nombre,unEmpleado.sexo,unEmpleado.sueldo,descSector);
 
 }
 
-void mostrarEmpleados(eEmpleados lista[],int tam)
+void mostrarEmpleados(eEmpleados lista[],int tam,eSector sectores[],int tamSector)
 {
     system("cls");
     printf("Legajo Nombre Sexo Sueldo\n\n");
@@ -113,10 +130,10 @@ void mostrarEmpleados(eEmpleados lista[],int tam)
    {
        if(lista[i].isEmpty==1)
        {
-           mostrarEmpleado(lista[i]);
-           //system("pause");
+           mostrarEmpleado(lista[i],sectores,tamSector);
        }
    }
+   system("pause");
 }
 
 
@@ -152,12 +169,13 @@ int buscarEmpleado(eEmpleados lista[],int tam,int legajo)
    return retorno;
 }
 
-void altaEmpleado(eEmpleados lista[],int tam)
+void altaEmpleado(eEmpleados lista[],int tam,eSector sectores[],int tamSector)
 {
     int legajo;
-    int libre,existe;
+    int libre,existe,idSector;
 
     eEmpleados empleado;
+
     libre= buscarLibre(lista,tam);
 
     if(libre == -1)
@@ -186,6 +204,11 @@ void altaEmpleado(eEmpleados lista[],int tam)
         printf("ingrese el sueldo: ");
         scanf("%f",&empleado.sueldo);
 
+        listarSectores(sectores,tamSector);
+        printf("ingrese el sector: ");
+        scanf("%d",&idSector);
+
+        empleado.idSector=idSector
         empleado.isEmpty=1;
         lista[libre]=empleado;
 
@@ -241,7 +264,172 @@ void bajaEmpleado(eEmpleados lista[],int tam)
 }
 
 
-void modificarEmpleado(eEmpleados lista[],int tam)
+void ordenarEmpleadosXlegajo(eEmpleado lista[], int tam)
 {
+    eEmpleado auxEmpleado;
+    for(int i=0; i<tam-1; i++){
+        for(int j= i+1; j < tam; j++){
+
+            if( lista[i].legajo > lista[j].legajo){
+                auxEmpleado = lista[i];
+                lista[i] = lista[j];
+                lista[j] = auxEmpleado;
+
+            }
+        }
+    }
+
+    printf("Empleados oredenados con exito!!!\n\n");
+
+
+}
+
+void ModificarEmpleado(eEmpleado lista[], int tam)
+{
+    int legajo;
+    int indice;
+    char seguir;
+    float nuevoSueldo;
+
+    system("cls");
+    printf("  *** Modificar  empleado ***\n\n");
+
+    printf("Ingrese legajo: ");
+    scanf("%d", &legajo);
+
+    indice = buscarEmpleado(lista, tam, legajo);
+
+    if( indice == -1)
+    {
+        printf("No hay ningun empleado de legajo %d\n\n", legajo);
+        system("pause");
+    }
+    else
+    {
+        mostrarEmpleado(lista[indice]);
+
+        printf("\nModifica sueldo? s/n: ");
+        fflush(stdin);
+        scanf("%c", &seguir);
+
+        if(seguir == 'n')
+        {
+            printf("Modificacion cancelada\n\n");
+
+        }
+        else
+        {
+            printf("Ingrese nuevo sueldo: ");
+            scanf("%f", &nuevoSueldo);
+            lista[indice].sueldo = nuevoSueldo;
+
+            printf("Modificacion exitosa\n\n");
+        }
+        system("pause");
+    }
+
+}
+
+void harcodearEmpleados(eEmpleados empleados[])
+{
+    eEmpleados lista[]={
+        {1111,"juan",'m',2,21000,1},
+        {2111,"luis",'m',3,23000,1},
+        {3111,"ana",'f',4,11000,1},
+        {4111,"mateo",'m',5,23000,1},
+        {5111,"mia",'f',6,91000,1},
+        {6111,"mauro",'m',1,16000,1},
+        {7111,"alberto",'m',1,31000,1},
+        {8111,"valentina",'f',2,22000,1},
+    };
+
+    for(int i=0;i<8;i++)
+    {
+        empleados[i]=lista[i];
+    }
+
+
+}
+void harcodearSectores{eSector sectores[]}
+{
+    eSector x[]={
+    {1,"RRHH"}
+    {2,"ciencia"}
+    {3,"sistemas"}
+    {4,"contable"}
+    {5,"social"}
+    {6,"quimico"}
+    }
+}
+
+void obtenerSector(eSector sectores[],int tam,int idSector,char cadena[])
+{
+    for(int=0;i<tam;i++)
+    {
+        if(sectores[i].id==idSector)
+        {
+            strcpy(cadena,sectores[i].descripcion);
+        }
+    }
+
+}
+
+
+void listarSectores(eSector sectores[],int tam)
+{
+    printf("id  descripcion");
+    for(int i=0,i<tam,i++)
+    {
+        printf("%d  %10s",sectores[i].id,sectores[i].descripcion);
+    }
+    printf("\n\n");
+}
+
+
+    int legajo;
+    int indice;
+    char seguir;
+    float nuevoSueldo;
+
+    system("cls");
+    printf("  *** Modificar  empleado ***\n\n");
+
+    printf("Ingrese legajo: ");
+    scanf("%d", &legajo);
+
+    indice = buscarEmpleado(lista, tam, legajo);
+
+    if( indice == -1)
+    {
+        printf("No hay ningun empleado de legajo %d\n\n", legajo);
+        system("pause");
+    }
+    else
+    {
+        mostrarEmpleado(lista[indice]);
+
+        printf("\nModifica sueldo? s/n: ");
+        fflush(stdin);
+        scanf("%c", &seguir);
+
+        if(seguir == 'n')
+        {
+            printf("Modificacion cancelada\n\n");
+
+        }
+        else
+        {
+            printf("Ingrese nuevo sueldo: ");
+            scanf("%f", &nuevoSueldo);
+            lista[indice].sueldo = nuevoSueldo;
+
+            printf("Modificacion exitosa\n\n");
+        }
+        system("pause");
+    }
+
+
+
+
 
 }
